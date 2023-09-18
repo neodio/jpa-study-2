0d923d7fe47af5388464cac94b52cfcd6c27cf79
enterfive <enterfive@gmail.com>
2023-09-18 18:59:30 +0900

    20230918 commit

---
+++ src/main/java/jpabook/jpastudy2/service/query/OrderQueryService.java
@@ -0,0 +1,27 @@
+package jpabook.jpastudy2.service.query;
+
+import jpabook.jpastudy2.domain.Order;
+import jpabook.jpastudy2.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class OrderQueryService {
+
+    private final OrderRepository orderRepository;
+
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return result;
+    }
+}
